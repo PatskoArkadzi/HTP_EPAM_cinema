f9f42155d8b5ee4e3f8b93c0496e904509f7053a
PatskoArkadzi <arkpatsko@gmail.com>
2018-08-08 23:15:11 +0300

    create GenreDao interface and GenreDaoImpl class

---
+++ src/main/java/by/htp/epam/cinema/db/dao/GenreDao.java
@@ -0,0 +1,7 @@
+package by.htp.epam.cinema.db.dao;
+
+import by.htp.epam.cinema.domain.Genre;
+
+public interface GenreDao extends BaseDao<Genre> {
+	//TODO add specific methods
+}
---
+++ src/main/java/by/htp/epam/cinema/db/dao/impl/GenreDaoImpl.java
@@ -0,0 +1,128 @@
+package by.htp.epam.cinema.db.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import by.htp.epam.cinema.db.dao.FilmDao;
+import by.htp.epam.cinema.db.dao.GenreDao;
+import by.htp.epam.cinema.db.pool.ConnectionPool;
+import by.htp.epam.cinema.domain.Film;
+import by.htp.epam.cinema.domain.Genre;
+
+public class GenreDaoImpl implements GenreDao {
+
+	private static Logger logger = LoggerFactory.getLogger(ConnectionPool.class);
+
+	private final String SQL_QUERY_GENRE_CREATE = "INSERT INTO `cinema_v2.0`.`genres` (`genreName`) VALUES (?);";
+	private final String SQL_QUERY_GENRE_READ = "SELECT `id`, `genreName` FROM `cinema_v2.0`.`genres` WHERE  `id`=?;";
+	private final String SQL_QUERY_GENRE_READ_ALL = "SELECT `id`, `genreName` FROM `cinema_v2.0`.`genres`;";
+	private final String SQL_QUERY_GENRE_UPDATE = "UPDATE `cinema_v2.0`.`genres` SET `genreName`=? WHERE  `id`=?;";
+	private final String SQL_QUERY_GENRE_DELETE = "DELETE FROM `cinema_v2.0`.`genres` WHERE  `id`=?;";
+
+	
+	// private final String SQL_QUERY_FILM_DELETE = "DELETE FROM
+	// `cinema_v2.0`.`films` WHERE `id`=?;";
+
+	@Override
+	public void create(Genre entity) {
+		Connection con = ConnectionPool.getConnection();
+		try (PreparedStatement ps = con.prepareStatement(SQL_QUERY_GENRE_CREATE)) {
+			ps.setString(1, entity.getGenreName());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			logger.error("SQLException in create method of GenreDaoImpl class", e);
+		} finally {
+			ConnectionPool.putConnection(con);
+		}
+	}
+
+	@Override
+	public Genre read(int id) {
+		ResultSet rs = null;
+		Connection con = ConnectionPool.getConnection();
+		try (PreparedStatement ps = con.prepareStatement(SQL_QUERY_GENRE_READ)) {
+			ps.setInt(1, id);
+			rs = ps.executeQuery();
+			if (rs.next())
+				return buildGenre(rs);
+		} catch (SQLException e) {
+			logger.error("SQLException in read method of GenreDaoImpl class", e);
+		} finally {
+			ConnectionPool.putConnection(con);
+			close(rs);
+		}
+		return null;
+	}
+
+	@Override
+	public List<Genre> readAll() {
+		List<Genre> genres = null;
+		ResultSet rs = null;
+		Connection con = ConnectionPool.getConnection();
+		try (Statement ps = con.createStatement()) {
+			rs = ps.executeQuery(SQL_QUERY_GENRE_READ_ALL);
+			genres = new ArrayList<>();
+			while (rs.next()) {
+				genres.add(buildGenre(rs));
+			}
+		} catch (SQLException e) {
+			logger.error("SQLException in readAll method of GenreDaoImpl class", e);
+		} finally {
+			ConnectionPool.putConnection(con);
+			close(rs);
+		}
+		return genres;
+	}
+
+	@Override
+	public void update(Genre entity) {
+		Connection con = ConnectionPool.getConnection();
+		try (PreparedStatement ps = con.prepareStatement(SQL_QUERY_GENRE_UPDATE)) {
+			ps.setString(1, entity.getGenreName());
+			ps.setInt(2, entity.getId());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			logger.error("SQLException in update method of GenreDaoImpl class", e);
+		} finally {
+			ConnectionPool.putConnection(con);
+		}
+	}
+
+	@Override
+	public void delete(Genre entity) {
+		Connection con = ConnectionPool.getConnection();
+		try (PreparedStatement ps = con.prepareStatement(SQL_QUERY_GENRE_DELETE)) {
+			ps.setInt(1, entity.getId());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			logger.error("SQLException in delete method of GenreDaoImpl class", e);
+		} finally {
+			ConnectionPool.putConnection(con);
+		}
+	}
+
+	private Genre buildGenre(ResultSet rs) throws SQLException {
+		Genre genre = new Genre();
+		genre.setId(rs.getInt("id"));
+		genre.setGenreName(rs.getString("genreName"));
+		return genre;
+	}
+
+	private void close(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				logger.error("SQLException in close method of GenreDaoImpl class", e);
+			}
+		}
+	}
+}
