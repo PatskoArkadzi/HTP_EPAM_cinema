52556b1ba6fec27bd3a4deeeae86ee7e9943018f
PatskoArkadzi <arkpatsko@gmail.com>
2018-08-12 20:47:25 +0300

    create custom tag

---
+++ src/main/java/by/htp/epam/cinema/web/tags/DisplayGenresBlockTag.java
@@ -0,0 +1,54 @@
+package by.htp.epam.cinema.web.tags;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import by.htp.epam.cinema.domain.Genre;
+import by.htp.epam.cinema.service.GenreService;
+import by.htp.epam.cinema.service.impl.GenreServiceImpl;
+import static by.htp.epam.cinema.web.util.constant.ContextParamNameConstantDeclaration.SESSION_PARAM_GENRELIST;
+
+public class DisplayGenresBlockTag extends TagSupport {
+
+	private static final long serialVersionUID = 6183940880542261086L;
+
+	private static Logger logger = LoggerFactory.getLogger(DisplayGenresBlockTag.class);
+
+	@SuppressWarnings("unchecked")
+	private List<Genre> getGenres() {
+		HttpSession session = pageContext.getSession();
+		List<Genre> genres = (List<Genre>) session.getAttribute(SESSION_PARAM_GENRELIST);
+		if (genres == null) {
+			GenreService genreService = new GenreServiceImpl();
+			genres = genreService.getAllGenres();
+			session.setAttribute(SESSION_PARAM_GENRELIST, genres);
+		}
+		return genres;
+	}
+
+	@Override
+	public int doStartTag() throws JspException {
+		List<Genre> genres = getGenres();
+		StringBuilder genresBlock = new StringBuilder();
+		genresBlock.append("<h5>Choose genre:</h5>");
+		for (Genre g : genres) {
+			genresBlock.append("<hr>").append("<a href=\"cinema?action=view_films_of_chosen_genre&user_chosen_genre_id=")
+					.append(g.getId()).append("\">").append(g.getGenreName()).append("</a>");
+		}
+		JspWriter out = pageContext.getOut();
+		try {
+			out.write(genresBlock.toString());
+		} catch (IOException e) {
+			logger.error("IOException in DisplayGenresBlockTag class", e);
+		}
+		return SKIP_BODY;
+	}
+}
--- src/main/java/by/htp/epam/cinema/web/util/constant/ContextParamNameConstantDeclaration.java
+++ src/main/java/by/htp/epam/cinema/web/util/constant/ContextParamNameConstantDeclaration.java
@@ -3,6 +3,7 @@ package by.htp.epam.cinema.web.util.constant;
 public final class ContextParamNameConstantDeclaration {
 
 	public static final String SESSION_PARAM_CURRENT_USER = "currentUser";
+	public static final String SESSION_PARAM_GENRELIST = "genrelist";
 
 	public static final String REQUEST_PARAM_ACTION = "action";
 	public static final String REQUEST_PARAM_FILM_WITH_GENRES = "filmWithGenres";
