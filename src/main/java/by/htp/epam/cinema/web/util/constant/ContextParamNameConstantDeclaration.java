3298bb13b6a26ebfc41f1c9d19ff9ddf0260f747
PatskoArkadzi <arkpatsko@gmail.com>
2018-08-12 16:11:47 +0300

    create some util constant declaration classes

--- src/main/java/by/htp/epam/cinema/web/util/constant/ContextParamNameConstantDeclaration.java
+++ src/main/java/by/htp/epam/cinema/web/util/constant/ContextParamNameConstantDeclaration.java
@@ -1,6 +1,8 @@
 package by.htp.epam.cinema.web.util.constant;
 
-public final class RequestParamNameConstantDeclaration {
+public final class ContextParamNameConstantDeclaration {
+
+	public static final String SESSION_PARAM_CURRENT_USER = "currentUser";
 
 	public static final String REQUEST_PARAM_ACTION = "action";
 	public static final String REQUEST_PARAM_FILM_WITH_GENRES = "filmWithGenres";
--- src/main/java/by/htp/epam/cinema/web/util/constant/PageNameConstantDeclaration.java
+++ src/main/java/by/htp/epam/cinema/web/util/constant/PageNameConstantDeclaration.java
@@ -3,5 +3,6 @@ package by.htp.epam.cinema.web.util.constant;
 public final class PageNameConstantDeclaration {
 
 	public static final String PAGE_USER_MAIN = "/WEB-INF/pages/user/home.jsp";
+	public static final String PAGE_ERROR = "/WEB-INF/pages/error.jsp";
 
 }
